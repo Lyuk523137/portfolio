51f0024cb71137ac0eac79690f5f9741b19f1f0a
Luke Marchenko <lyukmark1@gmail.com>
2025-10-08 16:39:23 +0200

    Contact button update

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,7 +4,7 @@ const Contact = () => {
     return (
     <div className="flex flex-col gap-10 items-center justify-center h-full">
         <div className="flex flex-row gap-4 items-center">
-            <h2 className="text-3xl font-extrabold text-black">Write me on What'sApp:</h2>
+            <h2 className="text-3xl font-extrabold text-black">Write me on What&apos;sApp:</h2>
             <h2 className="text-3xl font-bold text-white">+380637660322</h2>
          </div>
          <div className="flex flex-row gap-4 items-center">
